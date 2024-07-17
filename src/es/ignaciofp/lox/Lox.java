23f33eced2f11c8ea9e7ee4fdb9181723059a1a9
nafdez <fzp.nacho@gmail.com>
2024-07-17 19:45:32 +0200

    add interpreter

--- src/es/ignaciofp/lox/Lox.java
+++ src/es/ignaciofp/lox/Lox.java
@@ -10,7 +10,9 @@ import java.util.List;
 
 public class Lox {
 
+    private static final Interpreter interpreter = new Interpreter();
     public static boolean hadError = false;
+    public static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +30,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -52,7 +55,8 @@ public class Lox {
         Expr expression = parser.parse();
 
         if (hadError) return;
-        System.out.println(new AstPrinter().print(expression));
+        
+        interpreter.interpret(expression);
     }
 
     public static void error(int line, String message) {
@@ -65,6 +69,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    public static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
